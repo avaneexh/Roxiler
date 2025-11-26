6b798928ebea43bbda9bfb2e443ed72af5f27cd2
avaneesh <avaneesh.gupta5@gmail.com>
2025-11-26 17:34:07 +0530

    Fix: Update production base URL in env.js

--- client/src/env.js
+++ client/src/env.js
@@ -1,4 +1,4 @@
 export const env = {
  BASE_URL_DEV: "http://localhost:7000/api/v1",   
- BASE_URL_PRODUCTION: "https://roxilerbackend-seven.vercel.app/api/v1",   
+ BASE_URL_PRODUCTION: "https://roxilerbackend-b9x3.onrender.com/api/v1",   
 }
