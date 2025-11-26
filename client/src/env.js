ea831d2519fbe29f04e336046df8878b20a0c245
avaneesh <avaneesh.gupta5@gmail.com>
2025-11-26 16:44:08 +0530

    Fix production base URL in env.js by adding missing API path

--- client/src/env.js
+++ client/src/env.js
@@ -1,4 +1,4 @@
 export const env = {
  BASE_URL_DEV: "http://localhost:7000/api/v1",   
- BASE_URL_PRODUCTION: "https://roxilerbackend-seven.vercel.app",   
+ BASE_URL_PRODUCTION: "https://roxilerbackend-seven.vercel.app/api/v1",   
 }
